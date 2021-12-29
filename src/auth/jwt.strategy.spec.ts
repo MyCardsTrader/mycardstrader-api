564ec75ec7b021b36a5b7458f96fe3149280c15d
Benjamin Roullet <broullet@kaliopmail.com>
2021-12-29 15:24:01 -0800

    :white_check_mark: Add tests to jwt auth

---
+++ src/auth/jwt.strategy.spec.ts
@@ -0,0 +1,65 @@
+import { ExtractJwt } from "passport-jwt";
+import { JwtStrategy } from "./jwt.strategy";
+
+// jest.mock('passport-jwt', () => ({
+//   ...jest.requireActual('passport-jwt') as any,
+//   ExtractJwt: {
+//     fromAuthHeaderAsBearerToken: jest.fn(),
+//   },
+// }))
+
+describe('Jwt Strategy', () => {
+  let jwtStrategy;
+  const JWT_SECRET = 'JWT_SECRET_VALUE';
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+    jest.resetAllMocks();
+    process.env.JWT_SECRET = JWT_SECRET;
+
+    // extractJwtMock = jest.spyOn(ExtractJwt, 'fromAuthHeaderAsBearerToken');
+    // extractJwtMock.mockReturnValueOnce(() => null);
+
+    jwtStrategy = new JwtStrategy();
+  });
+
+  it('Should be defined', () => {
+    expect(jwtStrategy).toBeDefined();
+  });
+
+  describe('getStrategy', () => {
+    it('Should return stategy params', () => {
+      const result = JwtStrategy['getStrategy']();
+
+      expect(result).toStrictEqual({
+        jwtFromRequest: expect.any(Function),
+        ignoreExpiration: false,
+        secretOrKey: JWT_SECRET,
+      })
+    });
+  });
+
+  it('Should validate', async () => {
+    const result = await jwtStrategy.validate({
+      sub: 'subValue',
+    });
+
+    expect(result).toStrictEqual({
+      userId: 'subValue',
+    })
+  });
+
+  it('Should validate null', async () => {
+    const result = await jwtStrategy.validate({
+      sub: null,
+    });
+
+    expect(result).toStrictEqual({
+      userId: null,
+    })
+  });
+
+  it('Should not validate', async () => {
+    expect(jwtStrategy.validate(null)).rejects.toThrow("Cannot read property 'sub' of null");
+  });
+})
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -4,12 +4,18 @@ import { PassportStrategy } from '@nestjs/passport';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
-    super({
+
+  private static getStrategy() {
+    return {
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET,
-    });
+    }; 
+  }
+
+  /* istanbul ignore next */
+  constructor() {
+    super(JwtStrategy.getStrategy());
   }
 
   async validate(payload: any) {
